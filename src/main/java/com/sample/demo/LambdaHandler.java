fce68ffa7d35e60e2e82611f33cab40d2465b985
yasserajaj <infoyasser@gmail.com>
2024-08-29 07:53:42 +0800

    added aws Lambda function support

---
+++ src/main/java/com/sample/demo/LambdaHandler.java
@@ -0,0 +1,25 @@
+package com.sample.demo;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DemoApplication.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
+        return handler.proxy(awsProxyRequest, context);
+    }
+}
---
+++ src/main/java/com/sample/demo/entity/User.java
@@ -0,0 +1,12 @@
+package com.sample.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+	private String id;
+	private String firstName;
+	private String lastName;
+
+}
