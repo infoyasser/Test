e3872cf6bc966370c0d469df9ce780adf24ad599
yasserajaj <infoyasser@gmail.com>
2024-09-09 07:22:31 +0800

    added DefaultCredentialsProvider

--- src/main/java/com/sample/demo/config/S3Config.java
+++ src/main/java/com/sample/demo/config/S3Config.java
@@ -2,7 +2,7 @@ package com.sample.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -15,6 +15,7 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.AP_SOUTHEAST_1) // Adjust to your bucket's region
+                .credentialsProvider(DefaultCredentialsProvider.create())  // Ensure Lambda IAM role is used
                 .build(); // DefaultCredentialsProvider will automatically work in Lambda
     }
 }
