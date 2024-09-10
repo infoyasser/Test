67d8903f7d04554492dd7edc53f33917851aa4f6
yasserajaj <infoyasser@gmail.com>
2024-09-10 13:34:54 +0800

    Added bucketName

--- src/main/java/com/sample/demo/service/S3Service.java
+++ src/main/java/com/sample/demo/service/S3Service.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Service
 public class S3Service {
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final String bucketName = "sample-springboot3-tutorial "; // Replace with your bucket name
+    private final String bucketName = "sample-springboot3-tutorial"; // Replace with your bucket name
     private final S3Client s3Client;
 
     public S3Service() {
